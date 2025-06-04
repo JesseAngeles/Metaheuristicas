33d6efcc67d104b393fbe9a2bbbc2e40a91808ce
JesseAngeles <angeleslopezerickjesse@gmail.com>
2025-06-04 09:56:46 -0600

    MAO TIRA

--- Algorithms/Axolotl.py
+++ Algorithms/Axolotl.py
@@ -1,11 +1,13 @@
 import random
+import copy
 
 from Algorithms.Metaheuristic import Metaheuristic
 
 class Axolotl(Metaheuristic):
-    def __init__(self, problem, population_size = 10, damage = 0.5, regeneration = 0.1, tournament_size = 3, alpha = 0.5):
+    def __init__(self, problem, population_size = 10, male_size = 0.5, damage = 0.5, regeneration = 0.1, tournament_size = 3, alpha = 0.5):
         super().__init__(problem)
         self.population_size = population_size
+        self.male_size = int(self.population_size * male_size)
         self.damage = damage
         self.regeneration = regeneration
         self.torunament_size = tournament_size
@@ -19,61 +21,96 @@ class Axolotl(Metaheuristic):
         self.population = [self.problem.generateInitialSolution() for _ in range(self.population_size)]
 
     def optimize(self, epochs: int = 1):
-        for _ in range(epochs):
+        for _ in range(epochs):  
+            # Split population in male and female
             random.shuffle(self.population)
-            male = self.population[:int(self.population_size / 2)]
-            female = self.population[int(self.population_size / 2):]
-
-            best_male = self.bestIndividual(male)
-
-            male = [ self.updateSolution(best_male, larva) for larva in male ]
-            # for larva in self.population:
-            #     print(larva)
-            # print("")
-
-            for larva in male:
-                if random.random() < self.damage:
-                    for dimention in range(len(larva)):
-                        if random.random() < self.regeneration:
-                            larva = self.problem.getNextNeighbour(larva, dimention)
+            population = copy.deepcopy(self.population)
             
-
-            new_population = self.uniformCross(male, female)
-            self.population = self.elitism(self.population, new_population, self.problem.objective)
-            # for larva in self.population:
-            #     print(larva)
-            # print("")
-
-    def bestIndividual(self, population):
-        return max(population, key=lambda ind: self.problem.objective(ind))
-
-    def updateSolution(self, solution_1: list, solution_2: list):
-        solution: list = []
-        for i in range(len(solution_1)):
-            solution.append(solution_2[i] + self.alpha * (solution_1[i] - solution_2[i]))
-        return self.problem.normalizeSolution(solution)
-    
-    def uniformCross(self, male, female):
-        generation = []
-
-        for i in range(len(male)):
-            child1 = []
-            child2 =[]
+            males = population[:self.male_size]
+            females = population[self.male_size:]
             
-            for j in range(len(male[i])):
-                if random.random() < 0.5:
-                    child1.append(male[i][j])
-                    child2.append(female[i][j])
+            
+            # Transition male and female
+            self.transition(males)
+            self.transition(females)
+            
+            # Injury and restorations
+            self.injuryAndRestoration(males)
+            self.injuryAndRestoration(females)
+            
+            
+            
+            # Reproduction
+            new_population = self.reproduction(males, females)
+            
+            # Assortment
+            self.assortment(new_population)
+            
+    def transition(self, population):
+        best = self.bestIndividual(population)
+        values = [ self.problem.objective(individual) for individual in population ]
+        total = sum(values)
+
+        for i, individual in enumerate(population):
+            objective = values[i]
+            probability = objective / total if total else 0
+            r = random.random() / len(population)
+            
+            # print(f"p:{probability} - r:{r}")
+            
+            for j in range(len(individual)):
+                if probability < r and not self.compare(best, individual):
+                    individual[j] = individual[j] + (best[j] - individual[j]) * self.alpha
                 else:
-                    child1.append(female[i][j])
-                    child2.append(male[i][j])
+                    population[i] = self.problem.getRandomNeighbour(individual)
+                           
+            population[i] = self.problem.normalizeSolution(individual)
+
+
+    
+    def injuryAndRestoration(self, population):
+        for i, individual in enumerate(population):
+            if random.random() < self.damage:
+                for dimention in individual:
+                    if random.random() < self.regeneration:
+                        population[i] = self.problem.getNextNeighbour(individual, i)
+    
+    
+    def reproduction(self, males, females):
+        generation = []
+        for i in range(len(males)):
+            child1 = []
+            child2 = []
+            
+            for j in range(len(males[i])):
+                if random.random() < 0.5:
+                    child1.append(males[i][j])
+                    child2.append(females[i][j])
+                else:
+                    child1.append(females[i][j])
+                    child2.append(males[i][j])
             
             generation.append(child1)
             generation.append(child2)
-            
-        return generation
+        
+        return generation    
+        
+    def assortment(self, new_population: list):
+        combined = self.population + new_population
+        combined.sort(key=self.problem.objective, reverse=True)
+        self.population = combined[:len(self.population)]
     
-    def elitism(self, population: list, new_population: list, objective):
-        combined = population + new_population
-        combined.sort(key=objective)
-        return combined[:len(population)]
+    
+    def bestIndividual(self, population):
+        return max(population, key=lambda ind: self.problem.objective(ind))
+    
+    def printPopulation(self):
+        for i in self.population:
+            print(i, self.problem.objective(i))
+        print(" ")
+
+    def compare(self, sol1, sol2):
+        for i in range(len(sol1)):
+            if sol1[i] != sol2[i]:
+                return False
+        return True
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from Algorithms.Axolotl import Axolotl
 from Problems.KnapsackProblem import KnapsackProblem
 
 
-ksp = KnapsackProblem(10, 100)
+ksp = KnapsackProblem(20, 10)
 
 a = Axolotl(ksp)
-a.optimize()
-
-for _ in range(1000):
+# a.optimize()
+best = -9999
+for _ in range(50):
     a.optimize()
-    print(a.bestIndividual(a.population), ksp.objective(a.bestIndividual(a.population)))
+    a.printPopulation()    
+    print(a.bestIndividual(a.population), ksp.objective(a.bestIndividual(a.population)), "\n")
+    
